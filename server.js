7756d4f44d1faeea067df7b0cab466143da1ac73
mv0109 <21bcs119@iiitdmj.ac.in>
2025-06-25 14:06:53 +0530

    Fix: use dynamic port from process.env.PORT

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 // Middleware
 app.use(cors());
